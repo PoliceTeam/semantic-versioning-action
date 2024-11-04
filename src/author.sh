010698dd8482f68f7b022a62200918a4575448bf
Titus Pham <phamtien1709@gmail.com>
2024-11-04 11:20:07 +0700

    Update author

--- src/author.sh
+++ src/author.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------------------------------------------------------------
 # Filename:        index.sh
-# Author:          Nick (Ngoc Pham)
-# Created:         2023-10-23
-# Last Updated:    2024-08-30
+# Author:          Titus Pham
+# Created:         2024-11-04
+# Last Updated:    2024-11-04
 # Description:     Semantic versioning without any track files.
 # Version:         1.0.0
-# Github:          https://github.com/tuanngocptn
+# Github:          https://github.com/phamtien1709
 # Usage:           Read at https://github.com/PoliceTeam/semantic-versioning-action
 # Notes:           This action uses git tag for controlling the version. This action will not change/add/remove any file in your repository.
 # ------------------------------------------------------------------------------------------------------------------------------------------
